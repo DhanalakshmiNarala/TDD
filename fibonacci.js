c473e0abc8dd3fc726b6d9d3528b59ac95525956
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 12:30:19 +0530

    Refactor fibonnaci code for 1st unit test

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,14 @@
-module.exports = null
+const fibonnaciNumbers = (range)=> {
+    let result = []
+    for(let i=1; i<=range; i++)
+        result.push(nthFibonnaciNumber(i))
+    return result
+}
+
+const nthFibonnaciNumber = (n)=> {
+    if(n == 1 || n == 2)
+        return 1
+}
+
+
+module.exports = fibonnaciNumbers
