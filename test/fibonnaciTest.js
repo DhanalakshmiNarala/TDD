f390fdf506165ec96864d5b2cc17b6bda330b6bf
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 12:40:02 +0530

    Add fibonnaci 2nd and 3rd unit tests

--- test/fibonnaciTest.js
+++ test/fibonnaciTest.js
@@ -3,6 +3,12 @@ const should = require("chai").should()
 
 describe("Fibonnaci numbers", ()=> {
     it("should return 1 for first fibonnaci number", ()=> {
-        fibonnaciNumbers(1).should.deep.equal([1])
+        fibonnaciNumbers(1, 1).should.deep.equal([1])
+    })
+    it("should return 1 for second fibonnaci number", ()=> {
+        fibonnaciNumbers(2, 2).should.deep.equal([1])
+    })
+    it("should return 8 form 6th fibonnaci number", ()=> {
+        fibonnaciNumbers(8, 8).should.deep.equal([8])
     })
 })
