007631a11a18527a78a35d51534e3fbc9b9b89d2
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 11:46:35 +0530

    Add fizzbuzz 3rd unit test

--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -1,12 +1,15 @@
-const fizzBuzz = require('../fizzBuzz')
-const should = require('chai').should()
+const fizzBuzz = require("../fizzBuzz")
+const should = require("chai").should()
 
-describe('fizzBuzz', ()=> {
-    it('should return fizz for multiple numbers of 3', ()=> {
+describe("fizzBuzz", ()=> {
+    it("should return fizz for multiple numbers of 3", ()=> {
         fizzBuzz(6, 6).should.deep.equal(["fizz"])
     })
-    it('should return buzz for multiple numbers of 5', ()=> {
+    it("should return buzz for multiple numbers of 5", ()=> {
         fizzBuzz(10, 10).should.deep.equal(["buzz"])
+    })
+    it("should return fizzbuzz for multiple number of 3 and 5", ()=> {
+        fizzBuzz(15, 15).should.deep.equal(["fizzbuzz"])
     })
 })
     
