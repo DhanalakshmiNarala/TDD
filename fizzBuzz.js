931c1eeb9ffb814830a6500b0d9f0013c205c088
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 11:42:53 +0530

    Refactor the code for 2nd unit test

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -9,6 +9,8 @@ const fizzBuzz = (startRange, endRange)=> {
 const getText = (num)=> {
     if(num % 3 == 0)
         return "fizz"
+    if(num % 5 == 0)
+        return "buzz"
 }
 
 module.exports = fizzBuzz
