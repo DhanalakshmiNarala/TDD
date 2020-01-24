67c8e48df86c12c7bbc52cf412fc1ddee3fe67aa
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 12:48:00 +0530

    Refactor the fibonnaci code for 3rd unit test

--- test/fibonnaciTest.js
+++ test/fibonnaciTest.js
@@ -8,7 +8,15 @@ describe("Fibonnaci numbers", ()=> {
     it("should return 1 for second fibonnaci number", ()=> {
         fibonnaciNumbers(2, 2).should.deep.equal([1])
     })
-    it("should return 8 form 6th fibonnaci number", ()=> {
-        fibonnaciNumbers(8, 8).should.deep.equal([8])
+    it("should return 21 form 8th fibonnaci number", ()=> {
+        fibonnaciNumbers(8, 8).should.deep.equal([21])
+    })
+    it("shoul work for large range numbers", ()=> {
+        let expectedResult = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        fibonnaciNumbers(1, 10).should.deep.equal(expectedResult)
+    })
+    it("should work for very large range of numbers", ()=> {
+        let result = 53316291173
+        fibonnaciNumbers(53, 53).should.deep.equal([result])
     })
 })
