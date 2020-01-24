ca4d9e207ef5c849609cdb356cab4cd8b2e3670d
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-24 11:38:45 +0530

    Remove some logic for TDD way

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -7,13 +7,8 @@ const fizzBuzz = (startRange, endRange)=> {
 }
 
 const getText = (num)=> {
-    if(num % 3 == 0 && num %  5 == 0)
-        return "fizzbuzz"
     if(num % 3 == 0)
         return "fizz"
-    if(num % 5 == 0)
-        return "buzz"
-    return num
 }
 
 module.exports = fizzBuzz
--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -2,8 +2,11 @@ const fizzBuzz = require('../fizzBuzz')
 const should = require('chai').should()
 
 describe('fizzBuzz', ()=> {
-    it('should return fizz for multiple of 3', ()=> {
+    it('should return fizz for multiple numbers of 3', ()=> {
         fizzBuzz(6, 6).should.deep.equal(["fizz"])
+    })
+    it('should return buzz for multiple numbers of 5', ()=> {
+        fizzBuzz(10, 10).should.deep.equal(["buzz"])
     })
 })
     
